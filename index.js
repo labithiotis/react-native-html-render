d8309a1be351cb4f7372ddece175689edf953c8c
Darren Labithiotis <labithiotis@gmail.com>
2016-11-13 14:23:21 +0000

    Updated to newer react-native component method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {HtmlView, inlineTags, blockTags} from './lib/htmlRender'
+import { HtmlView, inlineTags, blockTags } from './lib/htmlRender';
 
 export const HtmlRender = HtmlView;
 export const inlineTagsList = inlineTags;
